1be8e1db2188e1b3d82df8abe54f2e85876633fe
henryjobel <jhobelsaha@gmail.com>
2023-09-25 04:58:17 +0600

    i created custom color style css using custom color and json file updated

--- src/Components/Donationcard/Donationcard.jsx
+++ src/Components/Donationcard/Donationcard.jsx
@@ -1,9 +1,11 @@
 
-
-const Donationcard = ({donationcard}) => {
+const Donationcard = ({ donationcard }) => {
+    const { category, category_details_view_color,title,donation_price,bg_color } = donationcard;
     return (
-        <div>
-            
+        <div  style={{ background: bg_color }}>
+            <h1 style={{ color: category_details_view_color }}>{title}</h1>
+            <h3 style={{ color: category_details_view_color }} >{donation_price}</h3>
+            <button className="btn btn-outline" style={{ color: category_details_view_color }}>{category}</button>
         </div>
     );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import Banner from '../../Components/Banner/Banner';
 import DonationCards from '../../Components/DonationCards/DonationCards';
 
